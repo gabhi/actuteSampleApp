445018c47971496c26ab87b1f9269c268b28270e
abhijit gaikwad <gaikwad.abhijit@gmail.com>
2014-06-03 15:00:18 -0700

    changed server configuration for testing

--- js/controllers/FolderListingCtrl.js
+++ js/controllers/FolderListingCtrl.js
@@ -53,7 +53,39 @@ angular.module('starter.controllers').controller('fileCtrl',
 );
 angular.module('starter.controllers').controller('visualsCtrl',
     function($scope, $http, $stateParams, $location, $rootScope, mySettings, dataService) {
+        var data = [4, 8, 15, 16, 23, 42];
 
+        var width = 420,
+            barHeight = 20;
+
+        var x = d3.scale.linear()
+            .domain([0, d3.max(data)])
+            .range([0, width]);
+
+        var chart = d3.select(".chart")
+            .attr("width", width)
+            .attr("height", barHeight * data.length);
+
+        var bar = chart.selectAll("g")
+            .data(data)
+            .enter().append("g")
+            .attr("transform", function(d, i) {
+                return "translate(0," + i * barHeight + ")";
+            });
+
+        bar.append("rect")
+            .attr("width", x)
+            .attr("height", barHeight - 1);
+
+        bar.append("text")
+            .attr("x", function(d) {
+                return x(d) - 3;
+            })
+            .attr("y", barHeight / 2)
+            .attr("dy", ".35em")
+            .text(function(d) {
+                return d;
+            });
     }
 );
 angular.module('starter.controllers').controller('bookmarksCtrl',
--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -2,13 +2,13 @@ var myApp = angular.module('starter.controllers', []);
 
 myApp.constant('mySettings', {
     appName: "Raise the bar!!",
-    loginUrl: 'http://agaikwad-pc.actuate.com:5000/ihub/v1/login',
-    getFolderListing: 'http://agaikwad-pc.actuate.com:5000/ihub/v1/folders',
-    getFileDetails: 'http://agaikwad-pc.actuate.com:5000/ihub/v1/files/',
-    visuals: 'http://agaikwad-pc.actuate.com:5000/ihub/v1/visuals/',
+    loginUrl: 'http://chezy01.actuate.com:5000/ihub/v1/login',
+    getFolderListing: 'http://chezy01.actuate.com:5000/ihub/v1/folders',
+    getFileDetails: 'http://chezy01.actuate.com:5000/ihub/v1/files/',
+    visuals: 'http://chezy01.actuate.com:5000/ihub/v1/visuals/',
     repositoryType: "Enterprise",
     jsapiUrl: "http://vmcip2.actuate.com:8700/iportal",
-    getReportPage: 'http://agaikwad-pc.actuate.com:5000/ihub/v1/getReportPage'
+    getReportPage: 'http://chezy01.actuate.com:5000/ihub/v1/getReportPage'
 });
 
 myApp.filter('escape', function() {
